c23d357c07ec661177d1aa756f8d25926c5ae1c2
Dickdick28 <dixonramos282005@hotmail.com>
2024-04-05 17:17:26 -0400

    Final esta vez si xd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Hola mundo", name: "Dixon", lastname : "Ramos", ci: "31073511" });
+  res.render('index', { title: "Hola mundo", name: "Dixon", lastname : "Ramos", ci: "31073511", section: "2"});
 });
 
 module.exports = router;

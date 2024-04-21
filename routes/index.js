67d2843f33375e3790157d8cee0f43d8e4a1523c
Dickdick28 <dixonramos282005@hotmail.com>
2024-04-20 23:26:25 -0400

    Final

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { name: "Dixon", lastname : "Ramos", ci: "31073511", section: "2"});
+  res.render('index', {skill1 : "HTML", skill2 : "CSS", skill3 : "JavaScript", skill4 : "Python", skill5 : "Nodejs", skill6 : "Django"});
 });
 
 module.exports = router;

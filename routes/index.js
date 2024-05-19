0465182e54bc49d71d326edd72930e6384bd1211
Dickdick28 <dixonramos282005@hotmail.com>
2024-05-19 15:42:56 -0400

    FINAL

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+const ContactosController = require("../controllers/ContactosController");
+const contactosController = new ContactosController();
+
+const indexController = require("../controllers/indexController")
+
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {skill1 : "HTML", skill2 : "CSS", skill3 : "JavaScript", skill4 : "Python", skill5 : "Nodejs", skill6 : "Django"});
-});
+router.get("/", indexController)
+
+router.post("/form-contacto", contactosController.add);
+
+
+
 
 module.exports = router;

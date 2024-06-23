f276f788a874fd37b45d25ac9af073c634d03c2a
Dickdick28 <dixonramos282005@hotmail.com>
2024-06-22 23:17:16 -0400

    Termine

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
   clientID: process.env.USER_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "https://p2-31073511.onrender.com/auth/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   return done(null, profile);
